f390e664e24cf0fa94497ccc3958b9c5dcb54e5f
burhan <burhanatique@gmail.com>
2022-05-10 16:39:00 +0500

    added the header component

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
 import * as ReactDOMClient from 'react-dom/client';
-
-const root = ReactDOMClient.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
-
-
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
+import ReactDOM from 'react-dom';
+// const root = ReactDOMClient.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
 // );
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
